403eb2de0e31eff9e8016d84bf8c000747d10fde
Илья Бондарчук <type1251@outlook.com>
2021-11-14 17:35:27 +1200

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+protected:
+	string name;
+	int age;
+
+public:
+	Animal(string name = "Animal", int age = 1) : name(name), age(age) {}
+
+	void voice(int questionmark) {
+		cout << "My name is " << name;
+		for (int i = 0; i < questionmark; i++)
+			cout << "?";
+
+	}
+};
+class Cat : virtual public Animal {
+protected:
+	string catname;
+	int trickery;
+public:
+	void voice(int meow) {
+		cout << "My name is " << catname;
+		for (int i = 0; i < meow; i++)
+			cout << " Meow!";
+		cout << endl;
+	}
+	Cat(string name = "Barsik", int age = 1, int trickery = 100) : catname(name), trickery(trickery) { }
+};
+class Dog : virtual public Animal {
+protected:
+	string dogname;
+	int loyalty;
+public:
+	void voice(int bark) {
+		cout << "My name is " << dogname;
+		for (int i = 0; i < bark; i++)
+			cout << " Bark!";
+		cout << endl;
+	}
+	Dog(string name = "Sharik", int age = 1, int loyalty = 100) : dogname(name), loyalty(loyalty) {}
+};
+class CatDog : public Cat, public Dog {
+public:
+	void information() {
+		cout << "Cat's name: " << catname << endl << "Dog's name: " << dogname << endl << "Cat's age: " << Cat::age << " Dog's age: " << Dog::age << "\nChance of trickery: " << Cat::trickery
+			<< " Chance of betrayal:" << Dog::loyalty << endl;
+		cout << "Allname:" << name << endl;
+		
+	}
+	CatDog(string allname = "Ivanova",string catname = "Barsik", int catage = 1, int trickery = 100, string dogname = "Sharik", int dogage = 1, int loyalty = 100) : Animal(allname, catage), Cat(catname, catage, trickery), Dog(dogname, dogage, loyalty) {}
+};
+void main() {
+	CatDog combined("Ivanovi", "Murzik", 2, 87, "Sharik", 2, 98);
+	combined.information();
+	combined.Cat::voice(6);
+	combined.Dog::voice(6);
+
+}
